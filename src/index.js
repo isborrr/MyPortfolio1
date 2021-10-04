a38025657031a3e5cf72851703a4078b2ccea6b7
Ismael Boro <isborrr@gmail.com>
2021-10-04 11:40:59 -0400

    second push

--- src/index.js
+++ src/index.js
@@ -14,4 +14,13 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// "scripts": {
+//   "predeploy": "npm run build",
+//   "deploy": "gh-pages -d build",
+//   "start": "react-scripts start",
+//   "build": "react-scripts build",
+//   "test": "react-scripts test",
+//   "eject": "react-scripts eject"
+// },
+
 reportWebVitals();
